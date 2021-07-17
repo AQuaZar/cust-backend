611b8f3d63b85ad23c330e32f0d6dbc98f6ed4cf
AQuaZar <greenskullcompany@gmail.com>
2021-07-18 02:02:26 +0300

    Fix serializer

--- cust_app/serializers.py
+++ cust_app/serializers.py
@@ -6,6 +6,7 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=100)
     description = serializers.CharField()
+    color = serializers.CharField()
     image = ImageField()
     slug = serializers.SlugField()
     price = serializers.FloatField()
