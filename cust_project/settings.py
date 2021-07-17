61264bb2740a67e7f4c0118e5a703a5526c4c7b7
AQuaZar <greenskullcompany@gmail.com>
2021-07-18 01:52:22 +0300

    Add migrations fix lib

--- cust_project/settings.py
+++ cust_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'pyuploadcare.dj',
+    'reset_migrations',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
